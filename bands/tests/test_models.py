af6783005e8f83e8939edcee8b0a6a9631e5f961
Tames McTigue <tames.mctigue@northwestern.edu>
2018-10-07 14:42:58 +0300

    Test that we can correctly save users, and cannot have duplicate email addresses, or missing email addresses

--- bands/tests/test_models.py
+++ bands/tests/test_models.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 
 from bands.models import CustomUser
 
@@ -8,5 +10,37 @@ class CustomUserModelTest(TestCase):
     def test_saving_and_retrieving_users(self):
         user = CustomUser()
         user.username = 'jjazz'
-        user.first_name = 'Jimmy Jazz'
+        user.name = 'Jimmy Jazz'
         user.email = 'jimmy.jazz@coolguy.com'
+        user.password = 'youneverknow8'
+        user.save()
+
+        new_user = CustomUser.objects.first()
+        self.assertEqual(user, new_user)
+
+    def test_cannot_save_without_email(self):
+        user = CustomUser()
+        user.username = 'jjazz'
+        user.name = 'Jimmy Jazz'
+        user.password = 'youneverknow8'
+        with self.assertRaises(ValidationError):
+            user.save()
+            user.full_clean()
+
+    def test_cannot_have_duplicate_email(self):
+        user1 = CustomUser()
+        user1.username = 'user1'
+        user1.name ='User One'
+        user1.password = 'IAmTheFirstUser'
+        user1.email = 'user@domain.com'
+        user1.save()
+        user1.full_clean()
+
+        user2 = CustomUser()
+        user2.username = 'user2'
+        user2.name = 'User Two'
+        user2.password = 'IAmTheSecondUser'
+        user2.email = 'user@domain.com'
+        with self.assertRaises(IntegrityError):
+            user2.save()
+            user2.full_clean()

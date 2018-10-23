ad222bbe63e519eb138161230fb4d31c13ffb6ca
Tames McTigue <tames@tamerz.com>
2018-10-23 20:48:03 +0300

    Simplifying since Heroku takes care of changing the settings file for us

--- bandfollow/settings.py
+++ bandfollow/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': 'bandfollow',
         'USER': 'bandfollow',
         'PASSWORD': 'bandfollow',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }

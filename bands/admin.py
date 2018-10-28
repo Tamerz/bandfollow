2f132e0a647a5ed994e9d4e1a5ec935fb2292bed
Tames McTigue <tames.mctigue@northwestern.edu>
2018-10-28 12:41:07 +0300

    Added better admin views

--- bands/admin.py
+++ bands/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from .models import Artist, Venue
 
 
-admin.site.register(Artist)
-admin.site.register(Venue)
+class ArtistAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'is_approved', 'submitted_by']
+    fields = ['name', 'is_approved', 'submitted_by']
+    list_filter = ['is_approved']
+
+
+class VenueAdmin(admin.ModelAdmin):
+
+    list_display = ['name', 'is_approved', 'submitted_by']
+    fields = ['name', 'is_approved', 'submitted_by']
+    list_filter = ['is_approved']
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Venue, VenueAdmin)
+

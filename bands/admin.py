c1e84b7ca4c633f970d5c231a9fc9f47965a9c2e
Tames McTigue <tames.mctigue@northwestern.edu>
2018-11-04 11:49:32 +0300

    Added events to admin interface

--- bands/admin.py
+++ bands/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Artist, Venue
+from .models import Artist, Venue, Event
 
 
 class ArtistAdmin(admin.ModelAdmin):
@@ -17,6 +17,13 @@ class VenueAdmin(admin.ModelAdmin):
     list_filter = ['is_approved']
 
 
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = ['title', 'date_and_time', 'is_approved']
+    fields = ['title', 'date_and_time', 'is_approved']
+    list_filter = ['is_approved']
+
+
 admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Venue, VenueAdmin)
-
+admin.site.register(Event, EventAdmin)
